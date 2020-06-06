0272c7d1d2b1ba91f616b1b35c18556c87be1f94
Harshitha Akkaraju <harshi.akkaraju@gmail.com>
2020-06-06 09:09:45 -0700

    code to read rss feed into a pandas dataframe

--- rss_feed_parser/main.py
+++ rss_feed_parser/main.py
@@ -2,7 +2,10 @@ from the_daily_parser import TheDailyParser
 
 def main():
     the_daily_parser = TheDailyParser()
-    the_daily_parser.parse_feed()
+    episode_data_df = the_daily_parser.episode_info_as_df()
+
+    THE_DAILY_DATA_FILE_NAME= "data/the_daily_episode_data.csv"
+    episode_data_df.to_csv(THE_DAILY_DATA_FILE_NAME, index=False)
 
 if __name__ == "__main__":
     main()
--- rss_feed_parser/the_daily_parser.py
+++ rss_feed_parser/the_daily_parser.py
@@ -1,5 +1,6 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+import pandas as pd
 
 THE_DAILY_RSS_FEED_URL = "http://rss.art19.com/the-daily"
 
@@ -7,21 +8,44 @@ class TheDailyParser:
     def __init__(self):
         self.url = THE_DAILY_RSS_FEED_URL
 
-    def parse_feed(self):
+    def __parse_feed(self):
         reader = urlopen(self.url)
         page = reader.read()
         reader.close()
 
         page_content = BeautifulSoup(page, "xml")
         item_list = page_content.findAll("item")
+        item_durations = page_content.findAll("itunes:duration")
 
-        for item in item_list:
+        daily_episodes = {}
+        dates = []
+        durations = []
+        descriptions = []
+        links = []
+        titles = []
+
+        for i, item in enumerate(item_list):
             title = item.title.text
             description = item.description.text
             date = item.pubDate.text
+            duration = item_durations[i].string
+            link = item.enclosure.get('url')
 
-            print(title)
-            print(description)
-            print(date)
-
-            break
+            dates.append(date)
+            descriptions.append(description)
+            durations.append(duration)
+            links.append(link)
+            titles.append(title)
+        
+        daily_episodes["date"] = dates
+        daily_episodes["duration"] = durations
+        daily_episodes["description"] = descriptions
+        daily_episodes["link"] = links
+        daily_episodes["title"] = titles
+        
+        return daily_episodes
+    
+    def episode_info_as_df(self):
+        daily_episodes = self.__parse_feed()
+        episode_data_df = pd.DataFrame.from_dict(daily_episodes)
+        return episode_data_df
