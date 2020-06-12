5cb7cd9b08cd9f333df5f31d33bdab2647348d61
Harshitha Akkaraju <harshi.akkaraju@gmail.com>
2020-06-12 12:29:12 -0700

    Starter model

--- nlp/main.py
+++ nlp/main.py
@@ -1,23 +1,28 @@
-import pandas as pd
+import fasttext
+import os
 
-THE_DAILY_DATA_FILE_PATH = "../rss_feed_parser/data/the_daily_episode_data.csv"
-TITLE_DESCRIPTION_DATA = "./data/the_daily_data.txt"
+def get_cbow_model(regenerate=False):
+    cbow_model_file_path = './models/cbow_model.bin'
+    if regenerate:
+        print("regenerating the cbow model...")
+        
+        data_file_path = "./data/the_daily_data.txt"
+        cbow_model = fasttext.train_unsupervised(data_file_path, model='cbow')
+        cbow_model.save_model(cbow_model_file_path)
+    
+    if os.path.exists(cbow_model_file_path):
+        model = fasttext.load_model(cbow_model_file_path)
+        return model
+    
+    return None
+
 
 def main():
-    episode_data_df = pd.read_csv(THE_DAILY_DATA_FILE_PATH)
-
-    # write title and description to a different file
-    titles = episode_data_df["title"].tolist()
-    descriptions = episode_data_df["description"].tolist()
-
-    writer = open(TITLE_DESCRIPTION_DATA,'w')
-    for i in range(0, len(titles)):
-        line = titles[i] + descriptions[i] + "\n"
-        writer.write(line)
+   cbow_model = get_cbow_model()
+   if cbow_model == None:
+       print("something went wrong...")
+       exit()
     
-    writer.close()
-
-
 
 if __name__ == "__main__":
     main()
